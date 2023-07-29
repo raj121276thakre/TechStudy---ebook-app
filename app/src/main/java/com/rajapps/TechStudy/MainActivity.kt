2437400e61158aba649b481ae45fa8d27d158120
raj121276thakre <raj121276thakre@gmail.com>
2023-07-29 12:47:44 +0530

    29/07/23 initial commit

--- app/src/main/java/com/rajapps/TechStudy/MainActivity.kt
+++ app/src/main/java/com/rajapps/TechStudy/MainActivity.kt
@@ -9,9 +9,9 @@ import com.rajapps.TechStudy.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
      lateinit var binding: ActivityMainBinding
 
-    //val activity = this
-   // val list: ArrayList<BooksModel> = ArrayList()
-   // val adapter = BooksAdapter(list, activity)
+    val activity = this
+    val list: ArrayList<BooksModel> = ArrayList()
+    val adapter = BooksAdapter(list, activity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,33 +20,24 @@ class MainActivity : AppCompatActivity() {
 
 
         //supportActionBar!!.hide()
-// code for bottom nav
-        val navHomeFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
-        val navController = navHomeFragment!!.findNavController()
-
-        val popupMenu = PopupMenu(this, null)
-        popupMenu.inflate(R.menu.bottom_menu)
-        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
-
-// code for bottom nav
 
 
-//        binding.apply {
-//            mRecyclerViewHome.adapter = adapter
-//
-//            list.add(BooksModel(R.drawable.m3_nirali,"M3 Nirali",getString(R.string.M3),"m3_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_1,"Rich Dad Poor Dad",getString(R.string.description_1),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_2,"Atomic Habits",getString(R.string.description_2),"se_bank.pdf"))
-//            list.add(BooksModel(R.drawable.book_3,"Best Self",getString(R.string.description_3),"se_bank.pdf"))
-//            list.add(BooksModel(R.drawable.book_4,"How To Be Fine",getString(R.string.description_4),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_5,"Out of the Box",getString(R.string.description_5),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_6,"Stripped",getString(R.string.description_6),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_7,"12 Rules for Life",getString(R.string.description_7),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_8,"Readistan",getString(R.string.description_8),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_9,"Reclaim Your Heart",getString(R.string.description_9),"sample_book.pdf"))
-//            list.add(BooksModel(R.drawable.book_10,"Lost Islamic History",getString(R.string.description_10),"sample_book.pdf"))
-//
-//        }
+        binding.apply {
+            mRecyclerViewHome.adapter = adapter
+
+            list.add(BooksModel(R.drawable.m3_nirali,"M3 Nirali",getString(R.string.M3),"m3_book.pdf"))
+            list.add(BooksModel(R.drawable.book_1,"Rich Dad Poor Dad",getString(R.string.description_1),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_2,"Atomic Habits",getString(R.string.description_2),"se_bank.pdf"))
+            list.add(BooksModel(R.drawable.book_3,"Best Self",getString(R.string.description_3),"se_bank.pdf"))
+            list.add(BooksModel(R.drawable.book_4,"How To Be Fine",getString(R.string.description_4),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_5,"Out of the Box",getString(R.string.description_5),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_6,"Stripped",getString(R.string.description_6),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_7,"12 Rules for Life",getString(R.string.description_7),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_8,"Readistan",getString(R.string.description_8),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_9,"Reclaim Your Heart",getString(R.string.description_9),"sample_book.pdf"))
+            list.add(BooksModel(R.drawable.book_10,"Lost Islamic History",getString(R.string.description_10),"sample_book.pdf"))
+
+        }
 
     }
 }
